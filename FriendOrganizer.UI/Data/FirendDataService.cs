fd1ef4492dd461d9f16400f24d8e1f35d2598f98
Lais Gomes <laisgomes2107@gmail.com>
2017-10-04 11:33:54 -0300

    Add project files.

---
+++ FriendOrganizer.UI/Data/FirendDataService.cs
@@ -0,0 +1,19 @@
+﻿using FriendOrganizer.Model;
+using System.Collections.Generic;
+
+namespace FriendOrganizer.UI.Data
+{
+    class FirendDataService : IFirendDataService
+    {
+        public IEnumerable<Friend> GetAll()
+        {
+            //TODO: Load data from real database
+
+            yield return new Friend{FirstName= "Alessandra", LastName="Brito"};
+            yield return new Friend{FirstName= "Juliana", LastName="Lima"};
+            yield return new Friend{FirstName= "Katyanne", LastName="Pantona"};
+            yield return new Friend{FirstName= "Lara Luisa", LastName="Gomes"};
+            
+        }
+    }
+}
